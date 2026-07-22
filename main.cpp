16d1348b4ebe3fd540a4d110b8eb5de332d87b51
kothm <k.othman.242@gmail.com>
2026-07-22 17:57:24 -0500

    more tasks but slower!

--- main.cpp
+++ main.cpp
@@ -15,27 +15,27 @@ int main()
     // Task1 -> run_vector();
 
     Temperature temp1(25);
-    auto Task2 = std::make_unique<Task> (&temp1, 15);
+    auto Task2 = std::make_unique<Task> (&temp1, 370);
     Task2 -> run();
     
     Pressure press1(37);
-    auto Task3 = std::make_unique<Task> (&press1, 20);
+    auto Task3 = std::make_unique<Task> (&press1, 280);
     Task3 -> run();
     
     mA mA1(98);
-    auto Task4 = std::make_unique<Task> (&mA1, 35);
+    auto Task4 = std::make_unique<Task> (&mA1, 420);
     Task4 -> run();
 
     Temperature temp2(1125);
-    auto Task5 = std::make_unique<Task> (&temp2, 15);
+    auto Task5 = std::make_unique<Task> (&temp2, 310);
     Task5 -> run();
     
     Pressure press2(1137);
-    auto Task6 = std::make_unique<Task> (&press2, 20);
+    auto Task6 = std::make_unique<Task> (&press2, 450);
     Task6 -> run();
     
     mA mA2(1198);
-    auto Task7 = std::make_unique<Task> (&mA2, 35);
+    auto Task7 = std::make_unique<Task> (&mA2, 260);
     Task7 -> run();
     
     while (true) std::this_thread::sleep_for(std::chrono::seconds(1));
