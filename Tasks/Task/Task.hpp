040eafdbcde39c4efac9ba0183596bb39bf28f9f
kothm <k.othman.242@gmail.com>
2026-07-22 20:29:52 -0500

    created a new derived class

--- Tasks/Task/Task.hpp
+++ Tasks/Task/Task.hpp
@@ -4,62 +4,57 @@
 #include <atomic>
 #include <chrono>
 
-#include "../Sensors/Sensor.hpp"
-
 class Task
 {
     private:
-        int _dt {10};
-        Sensor* _sensor;
         std::thread _thread;
-        std::atomic<bool> _running {false};
 
     protected:
-        void to_run()
-            {
-                if (!_sensor) //guard against nullptr constructor
-                {
-                    return; 
-                }
-                auto next = std::chrono::steady_clock::now();
-                const auto period = std::chrono::milliseconds(_dt);
-                while(_running)
-                {
-                    _sensor -> read();
-                    next += period;
-                    std::this_thread::sleep_until(next);
-                }
-            }
-        void join()
-            {
-                if (_thread.joinable())
-                {
-                    _thread.join();
-                }
-            }
+        Task() = default;
+
+        virtual void to_run() = 0; //the one thing derived classes have to provide, loops until _running is false
+
+        std::atomic<bool> _running {false};
+
     public:
-    
-        Task() : _sensor(nullptr) {} //to be able to inherit from Task
-        Task (Sensor *sensor, int dt) : _dt(dt), _sensor(sensor) {}
-        ~Task()
-            {
-                stop();
-            }
+        virtual ~Task()
+        {
+            stop();
+        }
+        
         void run()
+        {
+            if (_thread.joinable())
             {
-                if (_thread.joinable()) 
-                {    
-                    return;
-                }
-                    _running = true;
-                _thread = std::thread(&Task::to_run, this);
+                return;
             }
+            _running = true;
+            //_thread = std::thread([this] { to_run(); });
+            _thread = std::thread(&Task::to_run, this);
+        }
+
         void stop()
+        {
+            _running = false;
+            if (_thread.joinable())
             {
-                _running = false;
-                join();
+                _thread.join();
             }
-            
+        }
+
+        void join()
+        {
+            if (_thread.joinable())
+            {
+                _thread.join();
+            }
+        }
+        
+        bool is_running()
+        {
+            return _running;
+        }
+
         Task(const Task&) = delete;
         Task& operator=(const Task&) = delete;
 };
